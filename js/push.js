d69978ad546514e04ca006023f31b68e94db30c3
tyler-insight <tbramer@insightcreative.com>
2017-02-13 14:46:40 -0600

    Initial upload

---
+++ js/push.js
@@ -0,0 +1,35 @@
+$(function () {
+    var load = function (url) {
+        $.get(url).done(function (data) {
+            $("#content").html(data);
+        })
+    };
+
+    $(document).on('click', 'a', function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, 0);
+        var $this = $(this),
+            url = $this.attr("href"),
+            title = $this.text();
+
+        history.pushState({
+            url: url,
+            title: title
+        }, title, url);
+
+        document.title = title;
+
+        load(url);
+    });
+
+    $(window).on('popstate', function (e) {
+        var state = e.originalEvent.state;
+        if (state !== null) {
+            document.title = state.title;
+            load(state.url);
+        } else {
+            document.title = 'World Regions';
+            $("#content").empty();
+        }
+    });
+});
